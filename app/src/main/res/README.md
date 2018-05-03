8af1278ba9b6a1472ad41b4b74cb5c2a39c4c48d
WhitenAND07 <jordi14and@gmail.com>
2018-05-03 22:20:15 +0200

    Add and Modify Layouts

---
+++ app/src/main/res/README.md
@@ -0,0 +1,8 @@
+# Reversi Game in Android
+
+In this project we have done our first complex application for the subject "Apps for mobile devices" 
+that we are doing in the university of Lleida.
+
+## People that develop this app
+    Nil Agut - @nilagut
+    Jordi Blanco - @WhitenAND07
---
+++ app/src/main/res/app/src/main/java/com/android/nilagut/practicareversi/HelpActivity.java
@@ -0,0 +1,28 @@
+package com.android.nilagut.practicareversi;
+
+        import android.content.Intent;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.TextView;
+        import android.widget.Toolbar;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+        Button botoinici = (Button) findViewById(R.id.botoinici);
+        botoinici.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(v.getId() == R.id.botoinici){
+                    finish();
+                }
+            }
+        });
+    }
+}
+

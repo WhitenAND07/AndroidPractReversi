608aff1b7221a61416ae7d78d45eb2c8e7a602d5
WhitenAND07 <jordi14and@gmail.com>
2018-05-03 22:23:36 +0200

    Add and Modify Layouts

--- app/src/main/java/com/android/nilagut/practicareversi/HelpActivity.java
+++ app/src/main/java/com/android/nilagut/practicareversi/HelpActivity.java
@@ -8,20 +8,21 @@ package com.android.nilagut.practicareversi;
         import android.widget.TextView;
         import android.widget.Toolbar;
 
-public class HelpActivity extends AppCompatActivity implements View.OnClickListener {
+public class HelpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
         Button botoinici = (Button) findViewById(R.id.botoinici);
-        botoinici.setOnClickListener(this);
-    }
-
-    public void onClick(View v){
-        if(v.getId() == R.id.botoinici){
-            finish();
-        }
+        botoinici.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(v.getId() == R.id.botoinici){
+                    finish();
+                }
+            }
+        });
     }
 }
 
--- app/src/main/java/com/android/nilagut/practicareversi/opcions.java
+++ app/src/main/java/com/android/nilagut/practicareversi/opcions.java
@@ -7,17 +7,4 @@ package com.android.nilagut.practicareversi;
         public static String MIDA = "size";
         public static String TEMPS = "time";
         public static String GAMEBOARD = "gameBoard";
-
-        public static final String TAULER = "tauler";
-        public static final String PUNTUACIO1 = "jugador 1";
-        public static final String PUNTUACIO2 = "jugador 2";
-        public static final String SEGONS = "segons";
-        static final int SEGON = 1000;
-        static final int jugador1 = 1;
-        static final int jugador2 = 2;
-        public static final String esquerra = "esquerra";
-        public static final String dades = "dades";
-        public static final String log = "log";
-        public static final String email = "email";
-
  }

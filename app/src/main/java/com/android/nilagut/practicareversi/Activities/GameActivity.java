9bb879cbacd772bf0d9ea20a8d93ed4df144bcc6
WhitenAND07 <jordi14and@gmail.com>
2018-05-24 20:08:52 +0200

    Added Fragment Game (Not Completed)

--- app/src/main/java/com/android/nilagut/practicareversi/Activities/GameActivity.java
+++ app/src/main/java/com/android/nilagut/practicareversi/Activities/GameActivity.java
@@ -1,74 +1,25 @@
 package com.android.nilagut.practicareversi.Activities;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 import android.widget.GridView;
 
+import com.android.nilagut.practicareversi.Fragments.GameFragment;
 import com.android.nilagut.practicareversi.R;
 import com.android.nilagut.practicareversi.utils.GameBoard;
 import com.android.nilagut.practicareversi.utils.ImageAdapter;
 import com.android.nilagut.practicareversi.utils.Variables;
 
-public class GameActivity extends AppCompatActivity {
 
-    private int SIZE;
-    private boolean time;
-    private String player1;
-    private int countDown = 40;
+public class GameActivity extends FragmentActivity {
 
-    private TextView cells, timing, score1, score2;
 
-    private GameBoard gameBoard;
-    private GridView board;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle saveInstance) {
+        super.onCreate(saveInstance);
         setContentView(R.layout.activity_game);
-        getConfiguration();
-        if (savedInstanceState == null) initGame();
-        else getBackState(savedInstanceState);
-        initGridView();
-    }
-
-    private void initGridView() {
-        ImageAdapter imageAdapter = new ImageAdapter(this, gameBoard, player1, SIZE, time,
-                cells, timing, score1, score2);
-        this.board = (GridView) findViewById(R.id.board);
-        this.board.setAdapter(imageAdapter);
-        this.board.setBackgroundColor(getResources().getColor(R.color.green));
-        this.board.setNumColumns(SIZE);
-    }
-
-    private void getBackState(Bundle savedInstanceState) {
-        gameBoard = savedInstanceState.getParcelable(Variables.GAMEBOARD);
-        this.player1 = savedInstanceState.getString(Variables.USER);
-        this.SIZE = savedInstanceState.getInt(Variables.SIZE);
-        this.time = savedInstanceState.getBoolean(Variables.TIME);
-    }
-
-    private void initGame() {
-        gameBoard = new GameBoard(SIZE);
-        gameBoard.initGameBoard(time, countDown);
-    }
-
-    private void getConfiguration() {
-        player1 = getIntent().getStringExtra(Variables.USER);
-        SIZE = getIntent().getIntExtra(Variables.SIZE, 4);
-        time = getIntent().getBooleanExtra(Variables.TIME, false);
-        cells = (TextView) findViewById(R.id.cells);
-        timing = (TextView) findViewById(R.id.timing);
-        score1 = (TextView) findViewById(R.id.score1);
-        score2 = (TextView) findViewById(R.id.score2);
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putParcelable(Variables.GAMEBOARD, gameBoard);
-        outState.putString(Variables.USER, player1);
-        outState.putInt(Variables.SIZE, SIZE);
-        outState.putBoolean(Variables.TIME, time);
+        GameFragment gameF = (GameFragment) getSupportFragmentManager().findFragmentById
+                (R.id.fragmentGame);
     }
 }
---
+++ app/src/main/java/com/android/nilagut/practicareversi/Fragments/GameFragment.java
@@ -0,0 +1,90 @@
+package com.android.nilagut.practicareversi.Fragments;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.TextView;
+
+import com.android.nilagut.practicareversi.R;
+import com.android.nilagut.practicareversi.utils.GameBoard;
+import com.android.nilagut.practicareversi.utils.ImageAdapter;
+import com.android.nilagut.practicareversi.utils.Variables;
+
+
+public class GameFragment extends Fragment {
+
+    private String user;
+    private boolean time;
+    private int countDown = 40;
+    private int size;
+    private TextView cells, score1, score2, timing;
+    private GameBoard boardGame;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public View onCreateView (LayoutInflater inflater, ViewGroup container,
+                              Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.game_fragment, container, false);
+    }
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        getPreferences();
+        if (savedInstanceState != null) {
+            onRestoreInstanceState(savedInstanceState);
+        }
+        else {
+            boardGame = new GameBoard(size);
+            boardGame.initGameBoard(time, countDown);
+        }
+        startGridView();
+    }
+    private void getPreferences(){
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        user = prefs.getString(getResources().getString(R.string.user_key),
+                getResources().getString(R.string.user_default));
+        time = prefs.getBoolean(getResources().getString(R.string.time_key), true);
+        countDown = Integer.valueOf(prefs.getString(getResources().getString(R.string.selectTime_key),
+                "60"));
+        size = Integer.valueOf(prefs.getString(getResources().getString(R.string.board_key),
+                getResources().getString(R.string.board_Default_key)));
+        cells = (TextView) getView().findViewById(R.id.cells);
+        timing = (TextView) getView().findViewById(R.id.timing);
+        score1 = (TextView) getView().findViewById(R.id.score1);
+        score2 = (TextView) getView().findViewById(R.id.score2);
+    }
+
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        this.user = savedInstanceState.getString(getResources().getString(R.string.user_key));
+        this.time = savedInstanceState.getBoolean(getResources().getString(R.string.time_key));
+        this.countDown = savedInstanceState.getInt(getResources().getString(R.string.selectTime_key));
+        this.size = savedInstanceState.getInt(getResources().getString(R.string.board_key));
+        this.boardGame = savedInstanceState.getParcelable(Variables.GAMEBOARD);
+    }
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelable(Variables.GAMEBOARD, boardGame);
+        outState.putString(getResources().getString(R.string.user_key), user);
+        outState.putInt(getResources().getString(R.string.board_key), size);
+        outState.putBoolean(getResources().getString(R.string.time_key), time);
+        outState.putInt(getResources().getString(R.string.selectTime_key), countDown);
+    }
+    private void startGridView (){
+        ImageAdapter imageAdapter = new ImageAdapter(getActivity(), boardGame, user, size, time,
+                cells, timing, score1, score2);
+        GridView board = (GridView) getView().findViewById(R.id.board);
+        board.setAdapter(imageAdapter);
+        board.setBackgroundColor(getResources().getColor(R.color.green));
+        board.setNumColumns(size);
+    }
+}
